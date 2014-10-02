1d23f0d2c505ffadd2253938ca8cdcf89b1cd80e
felix <felix.casanellas@ravalnet.org>
2014-10-02 19:46:24 +0200

    Simplenews sending time

--- Xarxanet/sites/all/modules/simplenews_sending_time/simplenews_sending_time.js
+++ Xarxanet/sites/all/modules/simplenews_sending_time/simplenews_sending_time.js
@@ -7,16 +7,22 @@
 /**
  * Set scheduler info's display attribute to hide and show based on the option value.
  */
-Drupal.behaviors.simplenewsSendingTime = function (context) {
-  var simplenewsSendingTime = function () {
-    if($(".simplenews-command-send :radio:checked").val() == '5') {
-        $('.sending_time').css({display: "block"});
-    } else {
-      $('.sending_time').css({display: "none"});
-    }
-  }
 
-  // Update sending time display at page load and when a send option is selected.
-  $(function() { simplenewsSendingTime(); });
-  $(".simplenews-command-send").click( function() { simplenewsSendingTime(); });
-}
+(function ($) {
+	Drupal.behaviors.simplenewsSendingTime = {
+		attach: function (context, settings) {
+			var simplenewsSendingTime = function () {
+			    if($(".simplenews-command-send :radio:checked").val() == '5') {
+			        $('#edit-simplenews-sending-time').css({display: "block"});
+			    } else {
+			      $('#edit-simplenews-sending-time').css({display: "none"});
+			    }
+			}
+	
+			// Update sending time display at page load and when a send option is selected.
+			$(function() { simplenewsSendingTime(); });
+			$(".simplenews-command-send").click( function() { simplenewsSendingTime(); });
+		}
+	}
+})(jQuery);
+
