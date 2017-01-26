31928245306ad210be99b7c9c32dd2784c143a3a
Fèlix Casanellas <felix.casanellas@gmail.com>
2017-01-26 12:12:15 +0100

    Actualització del mòdul Webform Custom Simplenews

--- Xarxanet/sites/all/modules/webform_custom_simplenews/templates/custom-content-section.tpl.php
+++ Xarxanet/sites/all/modules/webform_custom_simplenews/templates/custom-content-section.tpl.php
@@ -1,79 +1,56 @@
 <?php
 $pathroot = 'http://www.xarxanet.org';
 $colors = array (
-		'noticia_ambiental' => '8ebc7c',
-		'noticia_comunitari' => 'f4bc45',
-		'noticia_cultural' => '959ed5',
-		'noticia_economic' => 'd1b91b',
-		'noticia_formacio' => 'be9255',
-		'noticia_informatic' => '9b8cd1',
-		'noticia_internacional_ang' => 'a75387',
-		'noticia_internacional_cat' => 'a75387',
-		'noticia_internacional_esp' => 'a75387',
-		'noticia_juridic' => '62aec5',
-		'noticia_projectes' => 'e39595',
-		'noticia_social' => 'b182ac' 
-);
-$ambits = array (
-		'noticia_ambiental' => 'Ambiental',
-		'noticia_comunitari' => 'Comunitari',
-		'noticia_cultural' => 'Cultural',
-		'noticia_economic' => 'Econòmic',
-		'noticia_formacio' => 'Formació',
-		'noticia_informatic' => 'Informàtic',
-		'noticia_internacional_ang' => 'Internacional',
-		'noticia_internacional_cat' => 'Internacional',
-		'noticia_internacional_esp' => 'Internacional',
-		'noticia_juridic' => 'Jurídic',
-		'noticia_projectes' => 'Projectes',
-		'noticia_social' => 'Social' 
+		'Ambiental' => '8ebc7c',
+		'Comunitari' => 'f4bc45',
+		'Cultural' => '959ed5',
+		'Econòmic' => 'd1b91b',
+		'Formació' => 'be9255',
+		'Informàtic' => '9b8cd1',
+		'Internacional' => 'a75387',
+		'Jurídic' => '62aec5',
+		'Projectes' => 'e39595',
+		'Social' => 'b182ac',
+		'General' => 'b1290b'  
 );
 $links = array (
-		'noticia_ambiental' => array (
+		'Ambiental' => array (
 				'ambiental',
 				'Més notícies ambientals' 
 		),
-		'noticia_comunitari' => array (
+		'Comunitari' => array (
 				'comunitari',
 				'Més notícies comunitàries' 
 		),
-		'noticia_cultural' => array (
+		'Cultural' => array (
 				'cultural',
 				'Més notícies culturals' 
 		),
-		'noticia_economic' => array (
+		'Econòmic' => array (
 				'economic/noticies',
 				'Més notícies econòmiques' 
 		),
-		'noticia_formacio' => array (
+		'Formació' => array (
 				'formacio/noticies',
 				'Més notícies sobre formació' 
 		),
-		'noticia_informatic' => array (
+		'Informàtic' => array (
 				'informatic/noticies',
 				'Més notícies informàtiques' 
 		),
-		'noticia_internacional_ang' => array (
-				'internacional/noticies/eng',
-				'Més notícies internacionals - Anglès' 
-		),
-		'noticia_internacional_cat' => array (
-				'internacional',
+		'Internacional' => array (
+				'internacional/noticies',
 				'Més notícies internacionals' 
 		),
-		'noticia_internacional_esp' => array (
-				'internacional/noticies/esp',
-				'Més notícies internacionals - Castellà' 
-		),
-		'noticia_juridic' => array (
+		'Jurídic' => array (
 				'juridic/noticies',
 				'Més notícies jurídiques' 
 		),
-		'noticia_projectes' => array (
+		'Projectes' => array (
 				'projectes/noticies',
 				'Més notícies sobre projectes' 
 		),
-		'noticia_social' => array (
+		'Social' => array (
 				'social',
 				'Més notícies socials' 
 		) 
@@ -84,8 +61,8 @@ $links = array (
 
 <table border="0" cellspacing="0" cellpadding="0" style="width: 580px; border-top: solid 20px #FFF; width: 580px;">
 	<tr>
-		<td style="background-color: <?php echo '#'.$colors[$section_name]?>; color: white; font-family: Georgia,Times New Roman,Times,serif; font-size: 12pt; padding: 3px 8px; height: 28px; width: <?php echo (strlen($ambits[$section_name]))*2.7;?>px"><?php echo $ambits[$section_name];?></td>
-		<td style="width: <?php echo 580-(strlen($ambits[$section_name]))*2.7;?>px; text-align: right;"><a 	href='<?php echo $links[$section_name][0]; ?>' 	style='font-family: Georgia, Times New Roman, Times, serif; color: #005577; font-size: 11pt; text-decoration: none;'><?php echo $links[$section_name][1]; ?></a></td>
+		<td style="background-color: <?php echo '#'.$colors[$section_name]?>; color: white; font-family: Georgia,Times New Roman,Times,serif; font-size: 12pt; padding: 3px 8px; height: 28px; width: <?php echo (strlen($section_name))*2.7;?>px"><?php echo $section_name;?></td>
+		<td style="width: <?php echo 580-(strlen($section_name))*2.7;?>px; text-align: right;"><a 	href='<?php echo $links[$section_name][0]; ?>' 	style='font-family: Georgia, Times New Roman, Times, serif; color: #005577; font-size: 11pt; text-decoration: none;'><?php echo $links[$section_name][1]; ?></a></td>
 	</tr>
 	<tr>
 		<td style="border-top: solid 2px <?php echo '#'.$colors[$section_name]?>; padding: 10px 5px;" colspan="2">
