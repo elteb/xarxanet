36aeaa26e58c71b69afcc362f40ebbe99551e942
El Teb <suport@elteb.org>
2017-04-10 15:09:11 +0200

    field_group_multiple updated

--- sites/all/modules/field_group_multiple/field-group-multiple-container.tpl.php
+++ sites/all/modules/field_group_multiple/field-group-multiple-container.tpl.php
@@ -1,19 +1,20 @@
 <?php
 /**
+ * @file
+ * TODO documentation
+ * 
  * - group
  * - items
  *  - delta + field_name + value render_array or null if empty
  */
 ?>
 <div class="field-group-multiple-container clearfix">
-	<div class="field-group-multiple-items">
-  <?php foreach($entries as $delta => $entry): ?>
-    <div
-			class="multiple-inline-element <?php print ($delta % 2 == 1 ? "odd" : "even"); ?> clearfix">
-    <?php foreach($entry as $field_name => $field): ?>
-      <div
-				class="multiple-element field-item-<?php print $field_name; ?>">
-      <?php if(!is_null($field)): ?>
+  <div class="field-group-multiple-items">
+  <?php foreach ($entries as $delta => $entry): ?>
+    <div class="multiple-inline-element <?php print ($delta % 2 == 1 ? 'odd' : 'even'); ?> clearfix">
+    <?php foreach ($entry as $field_name => $field): ?>
+      <div class="multiple-element field-item-<?php print $field_name; ?>">
+      <?php if (!is_null($field)): ?>
         <?php print render($field); ?>
       <?php else: ?>
         <span class="field-is-empty"> </span>
