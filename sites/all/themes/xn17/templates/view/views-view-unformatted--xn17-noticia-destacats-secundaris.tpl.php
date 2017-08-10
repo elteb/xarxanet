4fe78a73b82818f505553b6e12cd8e3818d58e15
JoelXN <joel.calvet@elteb.org>
2017-08-10 12:23:59 +0200

    Noticies destacades principal, secundaries i terciaries

--- sites/all/themes/xn17/templates/view/views-view-unformatted--xn17-noticia-destacats-secundaris.tpl.php
+++ sites/all/themes/xn17/templates/view/views-view-unformatted--xn17-noticia-destacats-secundaris.tpl.php
@@ -8,11 +8,9 @@
  */
 ?>
 <?php
-$view = views_get_current_view();
-print "<div class='titol-regio'>" . check_plain($view->get_title()) . "</div><div class='espaiblanc'></div>";
 $itRow = 1;
 foreach ($rows as $id => $row):  ?>
-  <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 pdng_mdl">
+  <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 pdng_mdl">
     <?php print $row; ?>
   </div>
 <?php
