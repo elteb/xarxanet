ff4763baad2856226892064081b6be2036955bf8
JoelXN <joel.calvet@elteb.org>
2017-08-11 12:47:26 +0200

    solucio errors git

--- sites/all/themes/xn17/templates/panel/fieldable-panels-pane--xn17_opinio_destacada_portada.tpl.php
+++ sites/all/themes/xn17/templates/panel/fieldable-panels-pane--xn17_opinio_destacada_portada.tpl.php
@@ -9,14 +9,14 @@
 <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 pdng_mdl">
     <div class="modul_2x1">
-      <div style="">Opinió</div>
+      <div class="titol">Opinió</div>
       <div class="cont_titular_entradeta">
     <?php
       print "<h4>" . $node->title . "</h4>";
       print $node->field_resum['und'][0]['value'];
     ?>
       </div>
-      <div style="border:solid 1px #aa0000;"><?php print $autor; ?></div>
+      <div class="pie"><?php print $autor; ?></div>
     </div>
   </div>
 </div>
