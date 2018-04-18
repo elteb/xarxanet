1d34d1f9ed55da2a5a81fb59de2e130a56ee7d04
El Teb <suport@elteb.org>
2018-04-18 11:25:50 +0200

    canvis als templates de producció

--- sites/all/themes/sasson_xarxanet/templates/nodes/node--butlleti_abast.tpl.php
+++ sites/all/themes/sasson_xarxanet/templates/nodes/node--butlleti_abast.tpl.php
@@ -59,7 +59,7 @@ $pathroot = 'http://www.xarxanet.org';
 
 // Banners fixes
 $banners_fixes = array(	'Banner PFVC' 	=> 	array (	$pathroot.'/sites/default/files/butlletins/abast/banner_PFVC.gif', 
-													'http://www.voluntariat.org/AgendadecursosdelPFVC.aspx'),
+													'https://voluntariat.gencat.cat/persones-voluntaries/format-com-a-voluntari/pla-de-formacio/'),
 						'Banner opinió' => 	array (	$pathroot.'/sites/default/files/butlletins/abast/banner_opinio.png', 
 													'http://bloc.xarxanet.org/'),
 						'Banner subs. butlletí' => 	array  ($pathroot.'/sites/default/files/butlletins/abast/banner_subsButlleti.png', 
@@ -77,7 +77,7 @@ $banners_fixes = array(	'Banner PFVC' 	=> 	array (	$pathroot.'/sites/default/fil
 						'Banner verso' => 	array  ($pathroot.'/sites/default/files/butlletins/abast/banner_verso.gif', 
 													'http://www20.gencat.cat/portal/site/bsf/menuitem.ce32baba3744d8fa172a63a7b0c0e1a0/?vgnextoid=cbc7b0b331fc6310VgnVCM2000009b0c1e0aRCRD&vgnextchannel=cbc7b0b331fc6310VgnVCM2000009b0c1e0aRCRD&vgnextfmt=default'),
 						'Banner masclisme' => 	array  ($pathroot.'/sites/default/files/butlletins/abast/banner_masclisme.gif', 
-														'http://www20.gencat.cat/portal/site/icdones/menuitem.351404635dde900639a72641b0c0e1a0/?vgnextoid=704754f598c9b110VgnVCM1000000b0c1e0aRCRD&vgnextchannel=704754f598c9b110VgnVCM1000000b0c1e0aRCRD&vgnextfmt=default&newLang=ca_ES'),
+														'http://dones.gencat.cat/ca/ambits/violencia_masclista/recursos_atencio/telefon_900/'),
 						'Banner blocs xarxanet' => 	array  ($pathroot.'/sites/default/files/butlletins/abast/banner_blocs.gif', 
 															'http://blocs.xarxanet.org/'),
 						'Banner omnia' => 	array  ($pathroot.'/sites/default/files/butlletins/abast/banner_omnia.gif', 
@@ -170,7 +170,17 @@ ksort($noticies);
 			<img src="http://www.gencat.cat/img/logo.gif" alt="logo Generalitat" style="border:0 none;"/>
 		</a>
 		<p style="text-align:right; vertical-align:bottom; margin-bottom:0px; margin-top:-10px">
-		Si no veieu correctament aquest butlletí, cliqueu <a style="font-weight:bold; color:#7b1b1c; text-decoration:none" href="http://www.voluntariat.org/Portals/0/Abast/documents/ultim_butlleti.html">aquí</a></p>	
+		Si no veieu correctament aquest butlletí, cliqueu <a style="font-weight:bold; color:#7b1b1c; text-decoration:none" href="
+		<?php 
+		if (is_numeric($title))	{
+			echo 'http://xarxanet.org/'.$title;
+		}	
+		?>
+		">aquí</a>
+		</p>
+
+		
+		
 		<table cellspacing="0" cellpadding="0" style="border-top: solid 3px #800000; width:100%">
 			<tr><?php 
 				if (is_numeric($title))	{
@@ -204,7 +214,7 @@ ksort($noticies);
 					?>
 				</td>
 				<td style="text-align: right; font-weight:bold">
-					<a style="color:#4D4D4D; text-decoration:none" href="http://www.voluntariat.org/abast.aspx">Butlletins anteriors</a>
+					<a style="color:#4D4D4D; text-decoration:none" href="http://xarxanet.org/hemeroteca-butlleti-labast">Butlletins anteriors</a>
 				</td></tr>
 			</table>
 		</td>
@@ -633,18 +643,18 @@ ksort($noticies);
 				<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/es/deed.ca"><img alt="Licencia de Creative Commons" src="http://i.creativecommons.org/l/by-nc-sa/3.0/es/88x31.png" style="border:0 none;"/></a>
 			</td></tr>
 		</table>	
-		<p style="font-size: 10px; text-align: justify; margin: 0">D'acord amb l’article 5.2 de la Llei orgànica 15/1999, de protecció de dades de caràcter personal, us informem que les dades personals han estat recollides, incorporades i tractades en un fitxer automatitzat les vostres , anomenat "Entitats de voluntariat", la finalitat del qual és trametre les comunicacions que el/la ciutadà/ana o l’entitat ha requerit . L'òrgan responsable del fitxer és la Direcció General d'Acció Cívica i Comunitària i l'adreça on la persona o entitat interessada pot exercir els drets d'accés, rectificació, cancel·lació i oposició és el Servei de Promoció de l’Associacionisme i el Voluntariat (Passeig del Taulat, 266-270, 08019, Barcelona) 
-		<br/>Aquest butlletí és una iniciativa del Departament de Benestar Social i Família de la Generalitat de Catalunya, coeditat amb la Fundació Pere Tarrés. ISSN: 1138-4352</p> 
+		<p style="font-size: 10px; text-align: justify; margin: 0"><a href="http://web.gencat.cat/ca/menu-ajuda/ajuda/avis_legal/">Avís legal</a>: D’acord amb l’article 17.1 de la Llei 19/2014, la &copy;Generalitat de Catalunya permet la reutilització dels continguts i de les dades sempre que se'n citi la font i la data d'actualització i que no es desnaturalitzi la informació (article 8 de la Llei 37/2007) i també que no es contradigui amb una llicència específica. Si l'adreça de correu que informeu al donar-vos d'alta deixa d'estar activa us donarem de baixa a la base de dades.
+		<br/>Aquest butlletí és una iniciativa del Departament de Treball, Afers Socials i Famílies de la Generalitat de Catalunya, coeditat amb la Fundació Pere Tarrés. ISSN: 2385-4146</p> 
 	</td>
 </tr><tr>
 	<td colspan="2" style="border-top: dotted 2px #800000; border-bottom: dotted 2px #800000; font-size:1px">&nbsp;</td>
 </tr><tr>
 	<td colspan="2">
 		<p style="font-size: 11px; margin: 0"> 
-			<a style="color: #800000; text-decoration: none" href="http://www.voluntariat.org/abast/formularialta.aspx">alta</a> ::  
-			<a style="color: #800000; text-decoration: none" href="http://www.voluntariat.org/abast/formularibaixa.aspx">baixa</a> ::   
+			<a style="color: #800000; text-decoration: none" href="http://xarxanet.org/sites/default/files/subscripcions_abast/alta.html">alta</a> ::  
+			<a style="color: #800000; text-decoration: none" href="http://xarxanet.org/sites/default/files/subscripcions_abast/baixa.html">baixa</a> ::   
 			<a style="color: #800000; text-decoration: none" href="mailto:alabast@voluntariat.org?Subject=Consulta%20butlletí">contacte</a> ::  
-			<a style="color: #800000; text-decoration: none" href="http://www.voluntariat.org/abast.aspx">butlletins anteriors</a>
+			<a style="color: #800000; text-decoration: none" href="http://xarxanet.org/hemeroteca-butlleti-labast">butlletins anteriors</a>
 		</p>
 	</td>
 </tr>
