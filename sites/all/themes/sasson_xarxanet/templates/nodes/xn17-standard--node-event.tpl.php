2686122e8f0f51a743af81d570c64ae6697fc2d8
TOTHOMweb <david.sabate@tothomweb.com>
2018-04-24 19:31:34 +0200

    correcció etiquetes agenda

--- sites/all/themes/sasson_xarxanet/templates/nodes/xn17-standard--node-event.tpl.php
+++ sites/all/themes/sasson_xarxanet/templates/nodes/xn17-standard--node-event.tpl.php
@@ -154,7 +154,8 @@
                     <ul class="links tags" role="navigation">
                     <?php
 						foreach($node->taxonomy_vocabulary_1['und'] as $tag) {;
-						    echo '<li>'.l( ucfirst($tag['taxonomy_term']->name), 'etiquetes/general/'.str_replace(' ', '-', $tag['taxonomy_term']->name)).'</li>';						} 
+              $term = taxonomy_term_load($tag['tid']);
+              echo '<li>'.l( ucfirst($term->name), 'etiquetes/general/'.str_replace(' ', '-', $term->name)).'</li>';  } 
                     ?>
                     </ul>
                 </div>
