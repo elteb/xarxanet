e4e6663421cf00c571288ed6e460153942d30003
Joan Galtés i Moreno <donbuche@gmail.com>
2020-03-02 17:49:50 +0100

    Implementant el paginador amb AJAX.

--- sites/all/modules/tothomweb_agenda/js/tothomweb_agenda.js
+++ sites/all/modules/tothomweb_agenda/js/tothomweb_agenda.js
@@ -9,11 +9,59 @@
 
 (function($) {
 
-  $(function() {
-    $('#tothomweb-agenda-form #edit-date').datepicker({
-      language: 'ca-ES',
-      format: 'dd-mm-yyyy'
-    });
-  });
+  Drupal.behaviors.tothomweb_agenda = {
+    attach: function (context, settings) {
+
+
+
+
+
+
+      $('#tothomweb-agenda-form #edit-date').datepicker({
+        language: 'ca-ES',
+        format: 'dd-mm-yyyy'
+      });
+
+
+
+
+
+
+      $('#tothomweb-agenda-form #more').click(function(e) {
+        e.preventDefault();
+
+        console.log('Ajax triggered!');
+        
+        var type = $(this).attr('data-type');
+        var search = $(this).attr('data-search');
+        var date = $(this).attr('data-date');
+        var location = $(this).attr('data-location');
+        var mode = $(this).attr('data-mode');
+        var page = $(this).attr('data-page');
+
+        $.ajax({
+          url: Drupal.settings.tothomweb_agenda.url,
+          method: 'POST',
+          data: {
+            'type': type,
+            'search': search,
+            'date': date,
+            'location': location,
+            'mode': mode,
+            'page': page
+          },
+          success: function(response) {
+            // console.log(response);
+            var $output = $($.parseHTML(response));
+            // $output = $output.find('#tothomweb-agenda-form #results');
+            console.log($output);
+            $('#tothomweb-agenda-form #results').empty();
+            $output.appendTo($('#tothomweb-agenda-form #results'));
+          }
+        });
+      });
+
+    }
+  };
 
 })(jQuery);
